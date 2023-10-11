e899921cc7cd5287b3069704505fc2bd16f8ed89
김주찬 <rlawncks125@naver.com>
2023-10-11 23:17:27 +0900

    openai Dalle-3 create Image 파일 자동 저장

--- backend/src/openai/openai.service.ts
+++ backend/src/openai/openai.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Response } from 'express';
+import { createWriteStream, writeFile } from 'fs';
 
 import OpenAI from 'openai';
+import { join } from 'path';
+import { Readable } from 'stream';
 
 // https://github.com/openai/openai-node/discussions/217
 
@@ -42,6 +45,42 @@ export class OpenaiService {
     });
     const url = response['data'][0]['url'];
 
+    // 파일 저장
+    const buf = await fetch(url).then(res => res.arrayBuffer())
+    const base64String = Buffer.from(buf).toString("base64");
+    this.wirteFileByBase64Image(base64String)
+        
+    
+
     res.send(url);
   }
+
+  async wirteFileByBase64Image(base64: string) {
+    new Date(Date.now()).toISOString();
+    const today = new Date(Date.now()).toISOString().split('T')[0];
+    const time = Date.now();
+
+    const fileName = `Dalle-2-${today}-${time}`;
+    const filePath = join(process.cwd(), `uploads/${fileName}.png`);
+
+    // data:image/png;base64,{base64-string} 형식일떄
+    // base64 데이터 추출
+ 
+
+    return new Promise((res, rej) => {
+      writeFile(filePath, base64, { encoding: 'base64' }, (err) => {
+        rej({
+          status: 'error',
+          err,
+        });
+      });
+
+      res({
+        status: 'success',
+      });
+    });
+  }
+
+  
+
 }
